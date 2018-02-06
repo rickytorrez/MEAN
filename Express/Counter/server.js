4f1705d23f4673b56ce995ef18a66768d7c11439
Ricardo <torrez.ricky7@gmail.com>
2018-02-06 00:24:05 -0500

    Working Counter

--- Express/Counter/server.js
+++ Express/Counter/server.js
@@ -1,3 +1,4 @@
+/******* BASIC ROUTING ******/
 var express     = require("express");
 var path        = require("path");
 var session     = require("express-session");
@@ -17,12 +18,29 @@ app.set('view engine', 'ejs');
 
 /******* ROUTE AND FUNCTION ******/
 
+/** HOME ROUTE AND COUNT BY 1 **/
 app.get('/', function(req, res){
     if (req.session.count == null){
         req.session.count = 1;
     } else {
         req.session.count = req.session.count + 1;
     }
+    console.log(req.session.count);
+    res.render("index", { count:req.session.count });
+})
+
+/** COUNT BY 2 **/
+app.get('/byTwo', function(req, res){
+    req.session.count += 1;
+    console.log(req.session.count);
+    res.redirect('/');
+})
+
+/** RESET COUNT **/
+app.get('/reset', function(req, res){
+    req.session.count = 0;
+    console.log(req.session.count);
+    res.redirect('/');
 })
 
 /******* PORT LISTEN ******/
