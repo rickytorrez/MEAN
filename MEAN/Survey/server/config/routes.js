9f9635995800304575da1a101c09efced083ee15
rickytorrez <torrez.ricky@ymail.com>
2018-02-13 17:16:10 -0500

    PostMan Working

--- MEAN/Survey/server/config/routes.js
+++ MEAN/Survey/server/config/routes.js
@@ -1,7 +1,10 @@
 const Users         = require('../controllers/users');
+const Polls         = require('../controllers/polls');
 
 module.exports      = function(app){
     app.post('/users', Users.create);
     app.delete('/users', Users.logout);
     app.get('/session', Users.session);
+    
+    app.post('/polls', Polls.create);
 }
