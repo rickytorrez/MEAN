6dead14a170f135cb2b4a5ce98e5b431fcfb1465
rickytorrez <torrez.ricky@ymail.com>
2018-07-05 21:31:58 -0400

    Controllers

--- FullMeanProjects/BicycleMarketplace/server/controllers/ListingController.js
+++ FullMeanProjects/BicycleMarketplace/server/controllers/ListingController.js
@@ -62,20 +62,115 @@ class ListingController {
 
     all(req, res){
 
+        // Populate in case user wants to contact whoever created the listing
+        Listing.find({})
+        .populate({
+
+            // populate .user (foreign field)
+            path:"user",
+
+            // use the model of the foreign key
+            model:"User"
+        })
+        .exec((err, listings)=>{
+            if(err){
+                return res.status(403).json({
+                    message:"Failed to populate listing's user for listing: " + listing._id,
+                    errors:err
+                });
+            } else {
+                return res.status(200).json(listings);
+            }
+        });
     }
 
     findById(req, res){
+        Listing.findOne({_id:req.params.id})
+        .populate({
 
+            // populate .user (foreign field)
+            path:"user",
+
+            // use the model of the foreign key
+            model:"User"
+        })
+        .exec((err, listing)=>{
+            if(err){
+                return res.status(403).json({
+                    message:"Failed to populate listing's user for listing: " + listing._id,
+                    errors:err
+                });
+            } else {
+                return res.status(200).json(listing);
+            }
+        });
     }
 
     update(req, res){
 
+        // finds the listing you want to update
+        Listing.findOne({_id:req.params.id}, (err,listing)=>{
+
+            // if the listing exists
+            if(listing){
+
+                // set the new values to what the form says or user old values if new ones are not provided
+                listing.title       = req.body.title || listing.title;
+                listing.description = req.body.description || listing.description;
+                listing.price       = req.body.price || listing.price;
+                listing.location    = req.body.location || listing.location;
+                listing.img         = req.body.img || listing.img;
+
+                listing.save(err=>{
+                    if(err){
+                        return res.status(403).json({
+                            message:"Failed to update listing: " + listing._id,
+                            errors:err
+                        });
+                    } else {
+                        return res.status(200).json(listing);
+                    }
+                });
+
+            } else {
+                return res.status(403).json({
+                    message: "Listing does not exist",
+                    errors:err
+                });
+            }
+        })
     }
 
     destroy(req, res){
 
+        //find the listing
+        Listing.findOne({_id:req.params.id}, (err, listing)=>{
+            if(listing){
+                
+                // remove the listing
+                Listing.remove({_id:req.params.id}, (err)=>{
+        
+                    // if we can't find the listing, send the error
+                    if(err){
+                        return res.status(404).json({
+                            message:"Failed to remove listing: " + req.params._id,
+                            errors: err
+                        });
+        
+                    // else return the listing(null, non existent)
+                    } else {
+                        return res.status(200).json(listing);
+                    }
+                });
+                
+            } else {
+                return res.status(404).json({
+                    message: "Failed to find listing: " + req.params.id,
+                    errors: err
+                });
+            }     
+        });
     }
-
 }
 
 module.exports = new ListingController();
