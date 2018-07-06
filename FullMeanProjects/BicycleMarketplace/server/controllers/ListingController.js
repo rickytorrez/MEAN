3a9a5a394649c161b11aecaa6a6a58caa649feb1
rickytorrez <torrez.ricky@ymail.com>
2018-07-05 21:06:29 -0400

    Controllers

--- FullMeanProjects/BicycleMarketplace/server/controllers/ListingController.js
+++ FullMeanProjects/BicycleMarketplace/server/controllers/ListingController.js
@@ -1,6 +1,63 @@
 class ListingController {
     create(req, res){
 
+        // checks if there's a user in session, if not, go away
+        if(!req.session.uid){
+            return res.status(403).json({
+                message:"You must be logged in to create a listing!",
+                errors:"You must be logged in to create a listing!"
+            });
+        }
+
+        // if you exist, 
+        // create a new instance of the  listing and fill if up with the body of the request
+        let listing = new Listing(req.body);
+
+        // before we save, we need to make sure to populate the foreign key
+        listing.user = req.session.uid;
+
+        // save listing
+        listing.save(err=>{
+
+            // if any errors are found
+            if(err){
+                return res.status(403).json({
+                    message:"Failed to save listing",
+                    error:"err"
+                });
+            } else {
+                
+                User.findOne({_id:req.session.uid},(err,user)=>{
+                    if(user){
+
+                        // push the listing into the user's listings array
+                        user.listings.push(listing._id);
+
+                        // save the user
+                        user.save(err=>{
+
+                            if(err){
+                                return res.status(403).json({
+                                    message:"Failed to find user!",
+                                    error: err
+                                });
+                            } else {
+                                return res.status(200).json(listing);
+                            }
+                            
+                        });
+                    } else {
+                        return res.status(404).json({
+                            message:"Failed to find user!",
+                            error: err
+                        });
+                    }
+                });
+
+                // else, save the listing and return the listing
+                return res.status(200).json(listing);
+            }
+        });
     }
 
     all(req, res){
--- FullMeanProjects/BicycleMarketplace/server/controllers/UserController.js
+++ FullMeanProjects/BicycleMarketplace/server/controllers/UserController.js
@@ -5,8 +5,10 @@ let bcrypt  = require("bcrypt");
 class UserController {
 
     register(req, res){
+
         // if email exists, change the email
         User.findOne({email:req.body.email}, (err, user)=>{
+            
             // if user exists, return 404
             if(user){
                 return res.status(403).json({
@@ -14,6 +16,7 @@ class UserController {
                     errors: errs
                 });
             } else {
+                
                 // bcrypt the password input by user
                 bcrypt.hash(req.body.password, 8, function (err, hash){
                     if(err){
@@ -22,18 +25,23 @@ class UserController {
                             errors: err
                         })
                     } else {
+                        
                         //set your password to the new hash password
                         user.password = hash;
+                        
                         // if user with email doesn't exist, we'll let you create a user if no errors
                         user.save(errs=>{
+                        
                         // if any errors on validation
                         if(errs){
                             return res.status(403).json({
                                 message: "Failed to save user!",
                                 errors: errs
                             });
+                        
                         // We need to return a response to the front end
                         } else {
+                            
                             // before user is saved, store it in session
                             req.session.uid = user._id;
                             return res.status(200).json(user);
@@ -46,17 +54,22 @@ class UserController {
     }
 
     login(req, res){
+        
         // finds user by email
         User.findOne({email:req.body.email}, (err,user)=>{
             if(user){
+                
                 // compares user input password to password saved in the db
                 bcrypt.compare(req.body.password, user.password, function(err,result){
+                    
                     // if the passwords match, send the whole user back
                     if(result){
+                        
                         // before it returns the whole user, it throws it into session
                         req.session.uid = user._id;
                         return res.status(200).json(user);
-                    // if the passwords don't match, send an error response
+                    
+                        // if the passwords don't match, send an error response
                     } else {
                         return res.status(403).json({
                             message: "Invalid Credentials!",
@@ -65,6 +78,7 @@ class UserController {
                     }
                 });
             } else {
+                
                 // if the email doesn't exist, send an error response
                 return res.status(403).json({
                     message: "No user with this email was found.",
@@ -75,6 +89,7 @@ class UserController {
     }
 
     logout(req, res){
+        
         // set session to undefined instead of null
         req.session.uid = undefined;
         return res.status(200).json({
@@ -83,21 +98,27 @@ class UserController {
     }
 
     findById(req, res){
+        
         // find by the user id
         User.findOne({_id:req.params.id})
+        
         // look for all the listings by this one person we're looking for
         // populate needs to be passed two things, the model that it should look into and the attribute you want to populate on set model
         .populate({
+            
             // path is the attribute name
             path:"listings",
+            
             // the model is the model that join refers to
             model:"Listing"
         })
         .exec((err, user)=>{
             if(user){
+                
                 // if everything went well, return the user object
                 return res.status(200).json(user);
             } else {
+                
                 // return the error plus user id
                 return res.status(404).json({
                     message: "Failed to populate listings for user: " + user._id,
